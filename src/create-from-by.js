e340d9a67e1a9ebf4fcc8b85e3452c65dc737dba
Nurbol <nzeinoldinov@gmai.com>
2023-06-08 20:35:33 +0600

    finish versin

--- src/create-from-by.js
+++ src/create-from-by.js
@@ -1,128 +1,130 @@
 'use strict';
+
 const body = document.body;
-document.body.style.background = 'rgba(25, 158, 211, 0.918)';
+
 const header = document.createElement('header');
-let divContainer= document.createElement('div');
+const divHeader = document.createElement('div');
 const heading = document.createElement('h1');
-divContainer.appendChild(heading);
 const par1 = document.createElement('p');
-divContainer.appendChild(par1);
+
 heading.textContent = 'CREATE AN ACCOUNT';
-divContainer.style.color = 'white'
-par1.innerHTML = 'we always keep your name and email address private';
-divContainer.className = 'zogolovok-container';
-header.append(divContainer);
-body.appendChild (header);
+par1.textContent = 'we always keep your name and email address private';
 
+divHeader.appendChild(heading);
+divHeader.appendChild(par1);
+header.appendChild(divHeader);
+body.appendChild(header);
 
+const form = document.createElement('form');
 
-const formContainer = document.createElement('form');
-let divContainer2 = document.createElement('div');
-divContainer2.className = 'conteit-container';
-
-let div1 = document.createElement('div');
+const div1 = document.createElement('div');
 div1.className = 'input-container';
-let inputTypeText1 = document.createElement('input');
-inputTypeText1.setAttribute('type', 'text');
-inputTypeText1.value = 'First Name';
-div1.appendChild(inputTypeText1);
 
-let inputTypeText2 = document.createElement('input');
-inputTypeText2.setAttribute('type', 'text');
-inputTypeText2.value = 'Last Name';
-div1.appendChild(inputTypeText2);
+const input1 = document.createElement('input');
+input1.type = 'text';
+input1.className = 'user-name';
+input1.tabIndex = 1;
+input1.placeholder = 'First name';
 
-document.body.appendChild(formContainer);
-formContainer.appendChild(divContainer2);
-divContainer2.appendChild(div1);
+const input2 = document.createElement('input');
+input2.type = 'text';
+input2.className = 'user-name';
+input2.tabIndex = 1;
+input2.placeholder = 'Last name';
 
-let div2 = document.createElement('div');
+div1.appendChild(input1);
+div1.appendChild(input2);
+form.appendChild(div1);
+
+const div2 = document.createElement('div');
 div2.className = 'input-container';
 
-let inputTypeText3 = document.createElement('input');
-inputTypeText3.setAttribute('type', 'text');
-inputTypeText3.value = 'Dister Name';
-div2.appendChild(inputTypeText3);
+const input3 = document.createElement('input');
+input3.type = 'text';
+input3.className = 'user-name';
+input3.tabIndex = 1;
+input3.placeholder = 'Dister name';
 
-let inputTypeText4 = document.createElement('input');
-inputTypeText4.setAttribute('type', 'email');
-inputTypeText4.value = 'Email Address';
-div2.appendChild(inputTypeText4);
+const input4 = document.createElement('input');
+input4.type = 'email';
+input4.className = 'user-name';
+input4.tabIndex = 1;
+input4.placeholder = 'Email Address';
 
-document.body.appendChild(formContainer);
-formContainer.appendChild(divContainer2);
-divContainer2.appendChild(div2);
- // пароли
- let div3 = document.createElement('div');
- div3.className = 'input-container'; 
-let inputTypeText5 = document.createElement('input');
-inputTypeText5.setAttribute('type', 'password');
-inputTypeText5.value = 'password';
-div3.appendChild(inputTypeText5);
+div2.appendChild(input3);
+div2.appendChild(input4);
+form.appendChild(div2);
 
-let inputTypeText6 = document.createElement('input');
-inputTypeText6.setAttribute('type', 'password');
-inputTypeText6.value = 'password confirmation';
-div3.appendChild(inputTypeText6);
-document.body.appendChild(formContainer);
-formContainer.appendChild(divContainer2);
-divContainer2.appendChild(div3);
-//// кнопки
-let div4 = document.createElement('div');
-div4.className = 'button';
-body.appendChild(div4);
-let inputTypeText7= document.createElement('input');
-let h2 = document.createElement('h2');
-div4.appendChild(h2);
-inputTypeText7 = document.createElement('input');
-inputTypeText7.setAttribute('type', 'radio');
-inputTypeText7.className = 'input';
-h2.appendChild(inputTypeText7);
-let label1 = document.createElement('label');
-let labelText1=document.createTextNode ('join As a Buyer');
-let par2 = document.createElement ('p');
-par2.className = 'par';
-let par2Text=document.createTextNode ('i am looking for a Name, Logo or Tagline for my busininess, brand or product');
-par2.appendChild(par2Text);
-div4.appendChild(par2);
+const div3 = document.createElement('div');
+div3.className = 'input-container';
+
+const input5 = document.createElement('input');
+input5.type = 'password';
+input5.className = 'user-name';
+input5.tabIndex = 1;
+input5.placeholder = 'Password';
+
+const input6 = document.createElement('input');
+input6.type = 'password';
+input6.className = 'user-name';
+input6.tabIndex = 1;
+input6.placeholder = 'Password confirmation';
+
+div3.appendChild(input5);
+div3.appendChild(input6);
+form.appendChild(div3);
+
+const div4 = document.createElement('div');
+div4.className = 'table';
+
+const h2 = document.createElement('h2');
+const radio1 = document.createElement('input');
+radio1.type = 'radio';
+radio1.name = 'color';
+const label1 = document.createElement('label');
+label1.textContent = 'Join As a Buyer';
+h2.appendChild(radio1);
 h2.appendChild(label1);
-h2.appendChild(labelText1);
-// 2кнопка
-let div5 = document.createElement('div');
-div5.className = 'button';
-body.appendChild(div5);
-let inputTypeText8= document.createElement('input');
-let h3 = document.createElement('h3');
-div5.appendChild(h3);
-inputTypeText8 = document.createElement('input');
-inputTypeText8.setAttribute('type', 'radio');
-inputTypeText8.className = 'input';
-h3.appendChild(inputTypeText8);
-let label2 = document.createElement('label');
-let labelText2=document.createTextNode ('join As a Creative or Marketplace Seller');
-let par3 = document.createElement ('p');
-let par3Text=document.createTextNode ('i plan ti submit name idees. Logo dessigns or sell names in Domain Marketplace');
-par3.className = 'par';
-par3.appendChild(par3Text);
-div5.appendChild(par3);
+div4.appendChild(h2);
+
+const p1 = document.createElement('p');
+p1.textContent = 'I am looking for a Name, Logo or Tagline for my business, brand or product';
+div4.appendChild(p1);
+form.appendChild(div4);
+
+const div5 = document.createElement('div');
+div5.className = 'table';
+
+const h3 = document.createElement('h3');
+const radio2 = document.createElement('input');
+radio2.type = 'radio';
+radio2.name = 'color';
+const label2 = document.createElement('label');
+label2.textContent = 'Join As a Creative or Marketplace Seller';
+h3.appendChild(radio2);
 h3.appendChild(label2);
-h3.appendChild(labelText2);
-// чекбокс для галочки
-let div6 = document.createElement ("div");
-body.appendChild(div6);
-div6.id= 'checkbox';
-let inputTypeText9 = document.createElement ("input");
-inputTypeText9.setAttribute ('type','checkbox');
-div6.appendChild(inputTypeText9);
-let label3 = document.createElement('label');
-let labelText10=document.createTextNode ('Allow Squadhelp to send marceting/promotional offers from time to time');
+div5.appendChild(h3);
+
+const p2 = document.createElement('p');
+p2.textContent = 'I plan to submit name ideas, logo designs or sell names in Domain Marketplace';
+div5.appendChild(p2);
+form.appendChild(div5);
+
+const div6 = document.createElement('div');
+div6.id = 'checkbox';
+
+const checkbox = document.createElement('input');
+checkbox.type = 'checkbox';
+
+const label3 = document.createElement('label');
+label3.textContent = 'Allow Squadhelp to send marketing/promotional offers from time to time';
+
+div6.appendChild(checkbox);
 div6.appendChild(label3);
-div6.appendChild(labelText10);
- // кнопка вход
- let div7 = document.createElement('div');
- body.appendChild(div7);
- div7.className = 'button';
- let button = document.createElement('button');
- div7.appendChild(button);
- let buttonText = document.createTextNode('Create account');
- button.appendChild(buttonText);
+form.appendChild(div6);
+
+const button = document.createElement('button');
+button.textContent = 'Create account';
+form.appendChild(button);
+
+body.appendChild(form);
